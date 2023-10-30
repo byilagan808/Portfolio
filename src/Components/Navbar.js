4de671c6467016c1a91587432a848a0943f46d8a
byilagan808 <b808ilagan@gmail.com>
2023-10-29 20:20:02 -1000

    First Commit(test)

---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+export const Navbar = () =>{
+
+    return(
+        <>
+        <nav className="navbar navbar-expand">
+            <div className="container">
+                <a href="" className="navbar-brand">Blake Ilagan</a>
+                <ul className="navbar-nav">
+                    <li classnam="nav-item">
+                        <a href="#" className="nav-link">
+
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div className="container"> 
+
+        hihihihihiih
+            
+        </div>
+           
+        </>
+    )
+
+    
+
+}  
