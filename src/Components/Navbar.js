12f7c7585268f0ac9308ea4e599c8695816ae3b2
byilagan808 <b808ilagan@gmail.com>
2023-10-29 21:44:28 -1000

    Implemented scalable navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,16 +2,31 @@ export const Navbar = () =>{
 
     return(
         <>
-        <nav className="navbar navbar-expand">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container">
-                <a href="" className="navbar-brand">Blake Ilagan</a>
-                <ul className="navbar-nav">
-                    <li classnam="nav-item">
-                        <a href="#" className="nav-link">
-
-                        </a>
-                    </li>
-                </ul>
+                <a href="#" className="navbar-brand">Blake Ilagan</a>
+                <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-label="Expand Navigation">
+                    <div className="navbar-toggler-icon"></div>
+                </button>
+                <div className="collapse navbar-collapse" id="nav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link active" aria-current="page">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">About Me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Resume</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <div className="container"> 
