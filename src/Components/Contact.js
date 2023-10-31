5d2397a8ea6d0c1272d13657143d8c174f9982de
byilagan808 <b808ilagan@gmail.com>
2023-10-30 14:54:25 -1000

    test

---
+++ src/Components/Contact.js
@@ -0,0 +1,24 @@
+export const Contact = () => {
+    return Contact (
+
+        <>
+        
+        
+        <div className="container" id="contact">
+            <div className="row">
+                <div className="col">
+                    hi contact me 
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+
+        </>
+
+
+    )
+
+
+}
---
+++ src/Components/Resume.js
@@ -0,0 +1,22 @@
+export const Resume = () => {
+    return Resume (
+
+        <>
+        
+        
+        <div className="container" id="resume">
+            <div className="row">
+                <div className="col">
+                    hi
+                </div>
+            </div>
+        </div>
+        
+        
+        </>
+
+
+    )
+
+
+}
