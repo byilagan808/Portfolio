816c6e95e182e7167e74c701854eec6c870ae46f
byilagan808 <b808ilagan@gmail.com>
2023-10-30 14:44:44 -1000

    test

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,22 @@
+export const AboutMe = () => {
+    return AboutMe (
+
+        <>
+        
+        
+        <div className="container" id="aboutme">
+            <div className="row">
+                <div className="col">
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+        </>
+
+
+    )
+
+
+}
