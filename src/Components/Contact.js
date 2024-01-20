9089054204d8e439c6b7405627fc525a85ab5975
byilagan808 <b808ilagan@gmail.com>
2024-01-19 15:00:41 -1000

    added linkedin icon

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,20 +3,17 @@ export const Contact = () => {
 
         <>
         
-        
 
-        {/* 
-        
         <div className="container" id="contact" style={{paddingTop: 100, paddingBottom: 100}}>
             <h1 style={{textAlign: 'center', textDecoration: 'underline'}}> CONTACT ME </h1>
             <div className="row">
                 <div className="col">
-                 
+                    <i class="fa-brands fa-linkedin"></i>
                     
                 </div>
             </div>
         </div>
-         */}
+
         </>
 
 
