7371152f403ef61b924a878271177f1815a088e5
byilagan808 <b808ilagan@gmail.com>
2024-01-19 14:44:13 -1000

    Implemented icon to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,8 @@ export const Contact = () => {
         <div className="container" id="contact" style={{paddingTop: 100, paddingBottom: 100}}>
             <div className="row">
                 <div className="col">
-                 
+
+                <i class="fa-brands fa-linkedin"></i>
                     
                 </div>
             </div>
