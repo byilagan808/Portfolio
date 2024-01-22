412e8e9fa5dd91f232be49d7822d86d5ace2ed74
byilagan808 <b808ilagan@gmail.com>
2024-01-22 01:20:09 -1000

    added icons with links to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,8 +8,17 @@ export const Contact = () => {
             <h1 style={{textAlign: 'center', textDecoration: 'underline'}}> CONTACT ME </h1>
             <div className="row">
                 <div className="col">
-                    <i class="fa-brands fa-linkedin"></i>
-                    
+                    <p>Blake25@hawaii.edu</p>
+                    <p>(808) 265 - 8557</p>
+                    <div className="icons">
+                        <a href="https://www.linkedin.com/in/blakeilagan/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="https://github.com/byilagan808" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://www.instagram.com/blake.ilagan?igsh=MXV1cm94dnNqYXZhcg%3D%3D&utm_source=qr" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                        
+                    </div>
+                </div>
+                <div className="col">
+
                 </div>
             </div>
         </div>
