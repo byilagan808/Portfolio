60f59f45e2baabf61788459efbe7c3193f8e50ab
byilagan808 <b808ilagan@gmail.com>
2024-02-12 16:25:12 -1000

    final v

--- src/Components/List.js
+++ src/Components/List.js
@@ -4,7 +4,7 @@ export const List = () => {
         <>
     
         <div className="card col" style={{margin:'10px', height:'100%'}}>
-            <img src="images/task.png" alt="" className="card-img-top" style={{width:'auto', height:'auto'}}/>
+            <img src="images/task.png" alt="" className="card-img-top" style={{width:'auto', height:'auto', display:'block'}}/>
             <div className="card-body">
                 <h5 className="card-tite">TaskBuddy</h5>
                 <p className="card-text">A Todo list website for students & Acts as a study aid</p>
